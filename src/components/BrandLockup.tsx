cc08cc71163b5553bd7c8fb9cc652fa3df9e7492
Managalabharathi Trust Admin <admin@mangalabharathitrust.in>
2026-04-05 13:23:45 +0530

    Fix TypeScript warning: Remove unused showTagline parameter

--- src/components/BrandLockup.tsx
+++ src/components/BrandLockup.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 
-export default function BrandLockup({ showTagline = true }: { showTagline?: boolean }) {
+export default function BrandLockup() {
   return (
     <Link
       href="/"
